e120f28df9c1a42a2e7e140073fc1ce6c725b6d3
Lenard Aquino <lenardaquino@gmail.com>
2025-02-22 15:51:02 +0800

    first commit

---
+++ blogs/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='authors'
+    )
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='comments'
+    )
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.post
